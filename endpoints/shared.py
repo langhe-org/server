3b01c578560366b41224e3b250dcc157f3fef3f5
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-03 21:19:06 -0400

    Added CORS policy

--- endpoints/shared.py
+++ endpoints/shared.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# TODO: should probably be in a config
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

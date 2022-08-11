bbd8434ca4ead26661c6afe5054d515b3cac691b
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-11 08:20:56 -0400

    Got appengine to run

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ python -m uvicorn main:app --reload
 ```
 
 Open [http://localhost:8000/docs](http://localhost:8000/docs) with your browser to see the docs.
+
+
+## Deploying
+```bash
+poetry export -f requirements.txt -o requirements.txt
+gcloud app deploy
+```
--- database.py
+++ database.py
@@ -5,10 +5,31 @@ from models.greenhouses_state import *
 from models.db_base import Base
 import os
 from sqlalchemy import create_engine
+import sqlalchemy
 
-DATABASE_CONNECTION_STRING = os.getenv('DATABASE_CONNECTION_STRING')
-# engine = create_engine(DATABASE_CONNECTION_STRING, future=True)
-engine = create_engine(DATABASE_CONNECTION_STRING, echo=True, future=True)
+# DATABASE_CONNECTION_STRING = os.getenv('DATABASE_CONNECTION_STRING')
+DATABASE_UNIX_SOCKET = os.getenv('DATABASE_UNIX_SOCKET')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+
+DATABASE_CONNECTION_SOCKET = sqlalchemy.engine.url.URL.create(
+    drivername="postgresql",
+    username=DATABASE_USERNAME,
+    password=DATABASE_PASSWORD,
+    database=DATABASE_NAME,
+    query={"host": DATABASE_UNIX_SOCKET},
+)
+engine = create_engine(
+    # DATABASE_CONNECTION_STRING,
+    DATABASE_CONNECTION_SOCKET,
+    echo=True,
+    future=True,
+    pool_size=5,
+    pool_recycle=1800, # 30 minutes
+)
+
 
 engine.connect()
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)

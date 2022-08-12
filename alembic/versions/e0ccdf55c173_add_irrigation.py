0b7dfb93e6b844484b9dcd63c33e96595564600b
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-08-12 07:01:46 -0400

    Update older failing migration

--- alembic/versions/e0ccdf55c173_add_irrigation.py
+++ alembic/versions/e0ccdf55c173_add_irrigation.py
@@ -7,6 +7,7 @@ Create Date: 2022-08-09 21:44:24.669605
 """
 from alembic import op
 import sqlalchemy as sa
+import enum
 
 
 # revision identifiers, used by Alembic.
@@ -16,14 +17,21 @@ branch_labels = None
 depends_on = None
 
 
+class EnvironmentState(enum.Enum):
+    Default = "Default"
+
+class ControlMode(enum.Enum):
+    automatic = "automatic"
+    manual = "manual"
+
 
 
 def upgrade() -> None:
-    op.add_column('greenhouse_state', sa.Column('irrigation_mode', sa.Enum('automatic', 'manual', name='controlmode'), nullable=True))
+    op.add_column('greenhouse_state', sa.Column('irrigation_mode', sa.Enum(ControlMode), nullable=True))
     op.execute("UPDATE greenhouse_state SET irrigation_mode = 'automatic'")
     op.alter_column('greenhouse_state', 'irrigation_mode', nullable=False)
 
-    op.add_column('greenhouse_state', sa.Column('irrigation_state', sa.Enum('Default', name='irrigationstate'), nullable=True))
+    op.add_column('greenhouse_state', sa.Column('irrigation_state', sa.Enum(EnvironmentState), nullable=True))
     op.execute("UPDATE greenhouse_state SET irrigation_state = 'Default'")
     op.alter_column('greenhouse_state', 'irrigation_state', nullable=False)
 
